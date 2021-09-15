8a3e3e10b6266916705cca7e24049b1af2723b9c
Zach D <68976965+Raisinaut@users.noreply.github.com>
2021-09-15 09:00:01 +0000

    haha Mark Rebillet in empty pool. Slightly improved UX.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from keep_alive import keep_alive
 from discord_components import ComponentsBot, Button, Select, SelectOption, ButtonStyle
 
 selected_game = None # used when modifying a game from selection menu
+modify_message_id = None # used to delete old message for refresh
 
 token = os.environ['TOKEN']
 bot = ComponentsBot(command_prefix = "!")
@@ -15,7 +16,7 @@ greetings = ["Hey there", "Greetings", "Konnichiwa", "What's up", "Hello", "Hey"
 help_message = """Commands:
 !hello, !hi, !hey ------- Simple greeting
 !add [game] --------- Add a game to the pool
-!modify -------------- Remove a game or change its played status 
+!modify --------------- Remove a game or change its played status 
 !remind -------------- Show this week's game
 !list ------------------- Display all games in the pool"""
 
@@ -32,9 +33,16 @@ async def on_ready():
 @bot.event
 async def on_button_click(interaction):
   global selected_game
+  global modify_message_id
   btn = interaction.custom_id
-
-  if selected_game != None:
+  
+  if btn == "cancel_button":
+    # delete last modify message
+    last_modify_msg = await interaction.channel.fetch_message(modify_message_id)
+    await last_modify_msg.delete()
+    modify_message_id = None
+  # Options that require selection
+  elif selected_game != None:
     print(f"Modified {selected_game}")
     if btn == "played_button":
       data_manager.change_played_status(selected_game, True)
@@ -47,7 +55,13 @@ async def on_button_click(interaction):
     if btn == "remove_button":
       data_manager.remove_game(selected_game)
       await interaction.respond(content = "I've removed " + selected_game + " from game pool.")
-      await interaction.channel.send(data_manager.get_list_string())
+      # show updated pool
+      embed = discord.Embed(
+        title = "Remaining Games :video_game:",
+        description = data_manager.get_list_string(),
+        color = discord.Color.blue()
+      )
+      await interaction.channel.send(embed=embed)
 
     selected_game = None
 
@@ -92,55 +106,78 @@ async def remind(ctx):
 
 @bot.command()
 async def list(ctx):
-  await ctx.channel.send(data_manager.get_list_string())
+  embed = discord.Embed(
+    title = "Game Pool :video_game:",
+    description = data_manager.get_list_string(),
+    color = discord.Color.blue()
+  )
+  if len(data_manager.get_list([db["games"]])) == 0:
+    embed.set_thumbnail(url = "https://c.tenor.com/qx2ywkzWiV0AAAAd/marc-rebillet-rebillet.gif")
+  await ctx.send(embed=embed)
 
 @bot.command()
 async def modify(ctx):
+  await ctx.message.delete()
   # Reset selected game to None
   global selected_game
+  global modify_message_id
   selected_game = None
 
   game_list = data_manager.get_list([db["games"]])
+
+  if modify_message_id != None:
+    last_modify_msg = await ctx.channel.fetch_message(modify_message_id)
+    await last_modify_msg.delete()
+    modify_message_id = None
 
   # Exit if list is empty
   if len(game_list) == 0:
     await ctx.send("There are no games to modify.")
     return
-  
+
   # Construct list of options
   option_list = []
   for i in game_list:
     option_list.append(SelectOption(label = i, value = i))
 
-  await ctx.send(
+  message = await ctx.send(
     "Select a game to modify",
     components = [
       # Row 1
-      Select(
-        placeholder ="Select a game",
-        options = option_list,
-        custom_id = "select"
-      ),
+      [
+        Select(
+          placeholder ="Select a game",
+          options = option_list,
+          custom_id = "select"
+        ),
+      ],
       # Row 2
       [
         Button(
           label = "Mark played",
           style = 3,
-          custom_id = "played_button",
+          custom_id = "played_button"
         ),
         Button(
           label = "Mark unplayed",
           style = 1,
-          custom_id = "unplayed_button",
+          custom_id = "unplayed_button"
         ),
         Button(
           label = "Remove",
           style = 4,
-          custom_id = "remove_button",
+          custom_id = "remove_button"
+        ),
+        Button(
+          label = "Cancel",
+          style = 2,
+          custom_id = "cancel_button"
         )
       ],
     ],
   )
+  # Save message id for later deletion
+  modify_message_id = message.id
 
 keep_alive()
 bot.run(token)
